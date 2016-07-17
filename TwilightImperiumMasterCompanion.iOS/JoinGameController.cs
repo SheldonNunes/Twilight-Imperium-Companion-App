ec8f29e2d74a36ad48478f894791834984f85292
Sheldon Nunes <sheldon.nunes02@gmail.com>
2016-07-17 17:24:43 +1200

    Checkpoint #1
    Initial Checkpoint

---
+++ TwilightImperiumMasterCompanion.iOS/JoinGameController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TwilightImperiumMasterCompanion.iOS
+{
+	partial class JoinGameController : UIViewController
+	{
+		public JoinGameController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
